62e561453bec63601ded10ae61318561a91e5429
Dinesh Maind <dinesh.maind@smartdatainc.net>
2025-05-13 17:37:41 +0530

    Sample App code

---
+++ MVC CRUD/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using MVC_CRUD.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserCrudService _userCrudService;
+        public HomeController(UserCrudService userCrudService)
+        {
+            _userCrudService = userCrudService;
+        }
+        public ActionResult Index()
+        {
+            var listofData = _userCrudService.GetAll();
+            return View(listofData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (_userCrudService.CreateUser(user))
+            {
+                ViewBag.Message = "Data Insert Successful";
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _userCrudService.GetById(id);
+            //var data1=_context.Users.FirstOrDefault(u => u.Id == id);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+
+            if (_userCrudService.EditUser(user))
+            {
+                return RedirectToAction("index");
+            }
+            ViewBag.Message = "Data Updation Unsuccessful";
+            return View(user);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var data =_userCrudService.GetById(id);
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            if (_userCrudService.DeleteUser(id))
+            {
+                ViewBag.Message = "Record Deleted Successfully";
+            }
+            else
+            {
+                ViewBag.Message = "Record Could not be deleted Deleted";
+            }
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ MVC CRUD/Services/UserCrudService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Services
+{
+    public class UserCrudService
+    {
+        private readonly MVCCRUDDBContext _context;
+
+        public UserCrudService()
+        {
+            _context = new MVCCRUDDBContext();
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+        public User GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(x => x.Id == id);
+        }
+        public bool ExistsUser(int id)
+        {
+            return _context.Users.Any(x => x.Id == id);
+        }
+        public bool CreateUser(User user)
+        {
+            if (!ExistsUser(user.Id))
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool EditUser(User user)
+        {
+            if (!ExistsUser(user.Id))
+            {
+                return false;
+            }
+            var data = _context.Users.Where(u => u.Id == user.Id).FirstOrDefault();
+            if (data != null)
+            {
+                data.PhoneNo = user.PhoneNo;
+                data.LastName = user.LastName;
+                data.FirstName = user.FirstName;
+                data.Email = user.Email;
+                data.Gender = user.Gender;
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+        public bool DeleteUser(int id)
+        {
+            var data = GetById(id);
+            if (data != null)
+            {
+                _context.Users.Remove(data);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
